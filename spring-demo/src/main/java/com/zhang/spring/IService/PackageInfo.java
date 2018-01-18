212c92b052a5901109a5e9c7bc6bd36f026538a6
zhangdelei <zhangdelei000@gmail.com>
2018-01-18 10:26:01 +0800

    整理spring cloud

---
+++ spring-demo/src/main/java/com/zhang/spring/IService/PackageInfo.java
@@ -0,0 +1,10 @@
+package com.zhang.spring.IService;
+
+/**
+ * @author zhangdl
+ * @version class v1.0
+ * @date 2017/9/25 7:44
+ */
+public
+class PackageInfo {
+}
---
+++ spring-demo/src/main/java/com/zhang/spring/controller/BaseController.java
@@ -0,0 +1,10 @@
+package com.zhang.spring.controller;
+
+/**
+ * @author zhangdl
+ * @version class v1.0
+ * @date 2017/9/25 7:42
+ */
+public
+class BaseController {
+}
